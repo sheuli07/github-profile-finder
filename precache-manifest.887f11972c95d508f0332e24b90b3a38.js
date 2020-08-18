b2ee525c179ab3499d4b619a4e3c589fb1d83aa7
sheuli <mondalsheuli07@gmail.com>
2020-08-18 21:48:52 +0530

    Updates

---
+++ precache-manifest.887f11972c95d508f0332e24b90b3a38.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c81f1be8d0b5ae2a1108be18f902899",
+    "url": "/github-profile-finder/index.html"
+  },
+  {
+    "revision": "d7c24c4e0b9de4f5d762",
+    "url": "/github-profile-finder/static/css/main.da911f11.chunk.css"
+  },
+  {
+    "revision": "a54fb9057586e6e84621",
+    "url": "/github-profile-finder/static/js/0.08353a03.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/github-profile-finder/static/js/0.08353a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481ac48e4d18a3e7c8e1",
+    "url": "/github-profile-finder/static/js/3.af0621c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-profile-finder/static/js/3.af0621c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d9ca882059db8eca59d",
+    "url": "/github-profile-finder/static/js/4.18e8777d.chunk.js"
+  },
+  {
+    "revision": "df5c97f37fcebf76eeac",
+    "url": "/github-profile-finder/static/js/5.37300c18.chunk.js"
+  },
+  {
+    "revision": "99d902aa71bea928665b",
+    "url": "/github-profile-finder/static/js/6.71f3cab8.chunk.js"
+  },
+  {
+    "revision": "d7c24c4e0b9de4f5d762",
+    "url": "/github-profile-finder/static/js/main.37ed216f.chunk.js"
+  },
+  {
+    "revision": "78eab8b5f6b226470c29",
+    "url": "/github-profile-finder/static/js/runtime-main.fb075dc6.js"
+  }
+]);
