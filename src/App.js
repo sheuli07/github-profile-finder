909c706290e3535d3e1aef5b80dceb9036d0e127
sheuli <mondalsheuli07@gmail.com>
2020-08-18 22:22:50 +0530

    modified code for netlify redirection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 const Home = lazy(() => import("./views/home"));
 const UDetails = lazy(() => import("./views/userDetails"));
 
@@ -11,6 +11,7 @@ function App() {
                 <Suspense fallback={<div />}>
                     <Route exact path="/home" component={Home}></Route>
                     <Route eaxct path="/details" component={UDetails}></Route>
+                    <Redirect to="/home" />
                 </Suspense>
             </Switch>
         </Router>
